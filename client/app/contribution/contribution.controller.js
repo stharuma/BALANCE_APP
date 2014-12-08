a2032d2a2c0a058e0ae55b7c2ee9d1a2e85a1290
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2014-12-08 15:57:17 -0500

    Added note title

--- client/app/contribution/contribution.controller.js
+++ client/app/contribution/contribution.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('kf6App')
-    .controller('ContributionCtrl', function($scope) {
+    .controller('ContributionCtrl', function($scope, $http, $stateParams) {
         var contributionId = $stateParams.contributionId;
 
         $scope.contribution = {};
 
-        $http.get('/api/contributions/' + contributionId).success(function(contribution) {
+        $http.get('/api/notes/' + contributionId).success(function(contribution) {
+        	console.log(contribution);
             $scope.contribution = contribution;
         });
     });
