e58572431562aa5f32c56ca6c732d5336032d6a2
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2014-12-08 13:12:09 -0500

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/kf6-dev'
+  },
+
+  seedDB: true
+};
