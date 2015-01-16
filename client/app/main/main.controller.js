fc6a83062973477bde17d94251fafd2ebc40ebfa
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2015-01-16 17:17:00 -0500

    Added coherent plan for login navigations

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('kf6App')
-  .controller('MainCtrl', function () {
-  });
+    .controller('MainCtrl', function($scope, Auth) {
+        $scope.isLoggedIn = Auth.isLoggedIn;
+    });
