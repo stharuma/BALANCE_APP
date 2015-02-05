29cb2787d036b1718707d4dcb8585740c3afe1c8
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2015-02-05 14:44:17 -0500

    Added server scripts

---
+++ bin/shutdown.sh
@@ -0,0 +1,20 @@
+#!/bin/bash                                                                     
+
+LOCK=./lock
+
+if [ `whoami` != 'root' ]; then
+    echo 'I am not a root.'
+    exit;
+fi
+
+if ! [ -e $LOCK ]; then
+	echo "already stopped.";
+	exit;
+else
+	rm lock;
+fi
+
+kill `ps -ef | grep kf6 | grep -v grep | awk '{print $2}'`
+kill `ps -ef | grep grunt | grep -v grep | awk '{print $2}'`
+
+
---
+++ bin/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash                                                                     
+
+LOCK=./lock
+
+if [ `whoami` != 'root' ]; then
+    echo 'I am not a root.'
+    exit;
+fi
+
+if [ -e $LOCK ]; then
+	echo "already running.";
+	exit;
+else
+	touch lock;
+fi
+
+((PORT=80 ATTACHMENTS_PATH=../attachments grunt serve:dist) >> log.\
+log) 2>> err.log &
+
+
