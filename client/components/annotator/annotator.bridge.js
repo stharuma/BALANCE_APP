b90e619575da6e1961008cfdb9c2977f7bbc68de
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2015-09-25 11:02:34 +0900

    Added basic annotation system

---
+++ client/components/annotator/annotator.bridge.js
@@ -0,0 +1,27 @@
+angular.module('kf6App').directive('annotatable', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+
+            var contents = angular.element(element).annotator();
+
+            Annotator.Plugin.KFPlugin = function(element) {
+                return {
+                    pluginInit: function() {
+                        this.annotator
+                            .subscribe("annotationCreated", function(annotation) {
+                                console.info("The annotation: %o has just been created!", annotation)
+                            })
+                            .subscribe("annotationUpdated", function(annotation) {
+                                console.info("The annotation: %o has just been updated!", annotation)
+                            })
+                            .subscribe("annotationDeleted", function(annotation) {
+                                console.info("The annotation: %o has just been deleted!", annotation)
+                            });
+                    }
+                }
+            };
+            contents.annotator('addPlugin', 'KFPlugin');
+        }
+    };
+});
