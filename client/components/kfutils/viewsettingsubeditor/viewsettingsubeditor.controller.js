b5ee3a0526ceef099c3f3ca79e469907037a986d
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2016-02-21 23:39:29 +0900

    Fixed the code checking problem

--- client/components/kfutils/viewsettingsubeditor/viewsettingsubeditor.controller.js
+++ client/components/kfutils/viewsettingsubeditor/viewsettingsubeditor.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('kf6App')
-    .controller('ViewsettingsubeditorCtrl', function($scope, $community, $http) {
+    .controller('ViewsettingsubeditorCtrl', function() {
 
     });
