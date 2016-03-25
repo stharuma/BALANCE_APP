6811f3b229acddb0e86fd3ef47913f74d4c57de5
Suresh Tharuma <stharuma@gmail.com>
2016-03-25 19:32:30 -0400

    added indentation in promisingidea.spec

--- client/app/promisingideas/promisingideas.spec.js
+++ client/app/promisingideas/promisingideas.spec.js
@@ -2,19 +2,19 @@
 
 describe('Controller: PromisingIdeasCtrl', function () {
 
- // load the controller's module
-  beforeEach(module('kf6App'));
+    // load the controller's module
+    beforeEach(module('kf6App'));
 
-  var PromisingIdeasCtrl, scope;
+    var PromisingIdeasCtrl, scope;
 
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    PromisingIdeasCtrl = $controller('PromisingIdeasCtrl', {
-      $scope: scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        PromisingIdeasCtrl = $controller('PromisingIdeasCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should ...', function () {
+        expect(1).toEqual(1);
     });
-  }));
-
-  it('should ...', function () {
-    expect(1).toEqual(1);
-  });
 });
