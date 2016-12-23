230ad8b0f6f3b441b8eb5628d81f4e4742f7eb58
Christian Perreault <christian.perreault@fse.ulaval.ca>
2016-12-22 23:09:05 -0500

    TypeError: path must be absolute or specify root to res.sendFile

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var errors = require('./components/errors');
+var path = require('path');
 
 module.exports = function(app) {
 
@@ -46,6 +47,8 @@ module.exports = function(app) {
     // All other routes should redirect to the index.html
     app.route('/*')
         .get(function(req, res) {
-            res.sendFile(app.get('appPath') + '/index.html');
+            res.sendFile(path.resolve(app.get('appPath')) + '/index.html');
         });
 };
+
+
