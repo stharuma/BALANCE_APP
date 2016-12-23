0fd51dcdf684b6ac16307ab7c92a37695f1114d4
Christian Perreault <christian.perreault@fse.ulaval.ca>
2016-12-23 14:07:03 -0500

    Adjustment of indentation according to project standards (EditorConfig)

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -5,32 +5,32 @@ var app = require('../../app');
 var User = require('./user.model');
 
 var user = new User({
-    provider: 'local',
-    userName: 'fakeUser',
-    email: 'test@test.com',
-    password: 'password'
+  provider: 'local',
+  userName: 'fakeUser',
+  email: 'test@test.com',
+  password: 'password'
 });
 
 describe('User Model', function() {
-    before(function(done) {
-        // Clear users before testing
-        User.remove().exec().then(function() {
-            done();
-        });
+  before(function(done) {
+    // Clear users before testing
+    User.remove().exec().then(function() {
+      done();
     });
+  });
 
-    afterEach(function(done) {
-        User.remove().exec().then(function() {
-            done();
-        });
+  afterEach(function(done) {
+    User.remove().exec().then(function() {
+      done();
     });
+  });
 
-    it('should begin with no users', function(done) {
-        User.find({}, function(err, users) {
-            users.should.have.length(0);
-            done();
-        });
+  it('should begin with no users', function(done) {
+    User.find({}, function(err, users) {
+      users.should.have.length(0);
+      done();
     });
+  });
 
   it('should fail when saving a duplicate user', function(done) {
     var userDuplicate = new User(user);
@@ -43,19 +43,19 @@ describe('User Model', function() {
   });
 
 
-    it('should fail when saving without an userName', function(done) {
-        user.userName = '';
-        user.save(function(err) {
-            should.exist(err);
-            done();
-        });
+  it('should fail when saving without an userName', function(done) {
+    user.userName = '';
+    user.save(function(err) {
+      should.exist(err);
+      done();
     });
+  });
 
-    it("should authenticate user if password is valid", function() {
-        return user.authenticate('password').should.be.true;
-    });
+  it("should authenticate user if password is valid", function() {
+    return user.authenticate('password').should.be.true;
+  });
 
-    it("should not authenticate user if password is invalid", function() {
-        return user.authenticate('blah').should.not.be.true;
-    });
+  it("should not authenticate user if password is invalid", function() {
+    return user.authenticate('blah').should.not.be.true;
+  });
 });
