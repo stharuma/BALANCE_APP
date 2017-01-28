59b6a373a1b16140072df20a630bdce1e827edb1
Christian Perreault <christian.perreault@fse.ulaval.ca>
2017-01-27 22:31:06 -0500

    fix: putting back the default Mongo database name (kf6)

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,7 +18,7 @@ module.exports = {
         uri: process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/kf6-tact'
+            'mongodb://localhost/kf6'
     },
 
     seedDB: true
