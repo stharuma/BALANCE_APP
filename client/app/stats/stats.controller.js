daa4af9604a839691ddc53c9e9e66f4e38766205
Christian Perreault <christian.perreault@fse.ulaval.ca>
2017-04-20 15:44:19 -0400

    Indent properly

--- client/app/stats/stats.controller.js
+++ client/app/stats/stats.controller.js
@@ -147,29 +147,29 @@ angular.module('kf6App')
 
                 });
 
-// à gérer avec la traduction...
+              // TODO i18n
 
-Highcharts.setOptions({
-        lang: {
-                loading: 'Chargement...',
-                months: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
-                weekdays: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
-                shortMonths: ['jan', 'fév', 'mar', 'avr', 'mai', 'juin', 'juil', 'aoû', 'sep', 'oct', 'nov', 'déc'],
-                exportButtonTitle: "Exporter",
-                printButtonTitle: "Imprimer",
-                rangeSelectorFrom: "Du",
-                rangeSelectorTo: "au",
-                rangeSelectorZoom: "Période",
-                downloadPNG: 'Télécharger en PNG',
-                downloadJPEG: 'Télécharger en JPEG',
-                downloadPDF: 'Télécharger en PDF',
-                downloadSVG: 'Télécharger en SVG',
-                resetZoom: "Réinitialiser le zoom",
-                resetZoomTitle: "Réinitialiser le zoom",
-                thousandsSep: " ",
-                decimalPoint: ','
-        }
-});
+              Highcharts.setOptions({
+                lang: {
+                  loading: 'Chargement...',
+                  months: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+                  weekdays: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
+                  shortMonths: ['jan', 'fév', 'mar', 'avr', 'mai', 'juin', 'juil', 'aoû', 'sep', 'oct', 'nov', 'déc'],
+                  exportButtonTitle: "Exporter",
+                  printButtonTitle: "Imprimer",
+                  rangeSelectorFrom: "Du",
+                  rangeSelectorTo: "au",
+                  rangeSelectorZoom: "Période",
+                  downloadPNG: 'Télécharger en PNG',
+                  downloadJPEG: 'Télécharger en JPEG',
+                  downloadPDF: 'Télécharger en PDF',
+                  downloadSVG: 'Télécharger en SVG',
+                  resetZoom: "Réinitialiser le zoom",
+                  resetZoomTitle: "Réinitialiser le zoom",
+                  thousandsSep: " ",
+                  decimalPoint: ','
+                }
+              });
 
                 $translate(['student',
                             'chartTitleContributions',
