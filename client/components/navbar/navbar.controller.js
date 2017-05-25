5e2780f2e0d59c396e0a9a50f05091a3c11787b4
Christian Perreault <christian.perreault@fse.ulaval.ca>
2017-05-25 16:38:31 -0400

    Display version number dynamically in the navigation bar.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,45 +1,54 @@
 'use strict';
 
 angular.module('kf6App')
-        .controller('NavbarCtrl', function($scope, $location, Auth, $http, $modal) {
-          $scope.menu = [{
-              'title': 'home',
-              'link': '/'
-            }];
-        $scope.isCollapsed = true;
-        $scope.isLoggedIn = Auth.isLoggedIn;
-        $scope.isAdmin = Auth.isAdmin;
-        $scope.getCurrentUser = Auth.getCurrentUser;
+  .controller('NavbarCtrl', function($scope, $location, Auth, $http, $modal) {
+    $scope.menu = [{
+      'title': 'home',
+      'link': '/'
+    }];
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
 
-        $scope.logout = function() {
-            Auth.logout();
-            $location.path('/login');
-        };
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
 
-        $scope.isActive = function(route) {
-            return route === $location.path();
-        };
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
 
-        $scope.openDialog = function(size) {
-            $modal.open({
-                animation: true,
-                templateUrl: 'VersionModalContent.html',
-                controller: 'VersionModalCtrl',
-                size: size
-            });
-        };
+    $scope.openDialog = function(size) {
+      $modal.open({
+        animation: true,
+        templateUrl: 'VersionModalContent.html',
+        controller: 'VersionModalCtrl',
+        size: size
+      });
+    };
 
-    });
+    $scope.loadVersion = function() {
+      if (!$scope.version) {
+        $http.get('api/version').success(function(res) {
+          $scope.version = res;
+        });
+      }
+    };
+    $scope.loadVersion();
+
+  });
 
 angular.module('kf6App')
-    .controller('VersionModalCtrl', function($scope, $http, $kfutil) {
-        $kfutil.mixIn($scope);
-        $scope.loadVersion = function() {
-            if (!$scope.version) {
-                $http.get('api/version').success(function(res) {
-                    $scope.version = res;
-                });
-            }
-        };
-        $scope.loadVersion();
-    });
+  .controller('VersionModalCtrl', function($scope, $http, $kfutil) {
+    $kfutil.mixIn($scope);
+    $scope.loadVersion = function() {
+      if (!$scope.version) {
+        $http.get('api/version').success(function(res) {
+          $scope.version = res;
+        });
+      }
+    };
+    $scope.loadVersion();
+  });
